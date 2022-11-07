811400a3667ea75c1815dd93b1cba3fb924261c8
DinosaurPower <lysym685@newschool.edu>
2022-11-07 16:46:35 -0500

    easiest motion script in the world

--- FriendlyDragon/Assets/Scripts/DragonMove.cs
+++ FriendlyDragon/Assets/Scripts/DragonMove.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragonMove : MonoBehaviour
+namespace Bananagodzilla
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+    public class DragonMove : MonoBehaviour
     {
-        
+
+        public float speed = 10f;
+        private static readonly int MovementSpeed = Animator.StringToHash("MovementSpeed");
+        public float sc = 1;
+        float moveSpeed = 0f;
+
+        public Rigidbody2D rb;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            moveSpeed = Input.GetAxisRaw("Horizontal")*speed+Input.GetAxisRaw("Vertical")*speed;
+            transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime*speed, Input.GetAxis("Vertical") * Time.deltaTime * speed,  0f);
+
+       
+            
+            
+            
+
+        }
     }
 }
