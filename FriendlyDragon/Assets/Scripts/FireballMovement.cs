bfcd79d2e8cf3dc2bba5eca43c1882ee81de48fb
DinosaurPower <lysym685@newschool.edu>
2022-11-09 16:04:06 -0500

    ITS HERE SINCE WEDNESDAY ITS FIREBALL EDIT

--- FriendlyDragon/Assets/Scripts/FireballMovement.cs
+++ FriendlyDragon/Assets/Scripts/FireballMovement.cs
@@ -14,15 +14,19 @@ namespace Bananagodzilla
         public Rigidbody2D rigidbody;
         public float DeathTime;
         public bool Right;
+        public DragonMove move;
 
-       
+        public float DragonDir;
         // Start is called before the first frame update
         void Start()
-        {
+        { move = FindObjectOfType(typeof(DragonMove)) as DragonMove;
+
+            DragonDir = move.GetComponent<Rigidbody2D>().velocity.x;
             rigidbody = GetComponent<Rigidbody2D>();
+              rigidbody.velocity = transform.right * projectileSpeed*DragonDir;
             if (Right)
             {
-                rigidbody.velocity = transform.right * projectileSpeed;
+                rigidbody.velocity = transform.right * projectileSpeed*DragonDir;
             }
 
             if (!Right)
