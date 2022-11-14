f99ece46676dc6b9533787dee705f2ce78a14a5c
DinosaurPower <lysym685@newschool.edu>
2022-11-14 17:14:27 -0500

    I FOUND HOW TO FIX CANVAS also added resource counters

--- FriendlyDragon/Assets/Scripts/Crafting/Item.cs
+++ FriendlyDragon/Assets/Scripts/Crafting/Item.cs
@@ -1,15 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Bananagodzilla
 {
     public class Item : MonoBehaviour
     {
+        public TMP_Text text;
         public string ItemName;
 
         public int ItemAmount = 100;
         public GameObject projectile;
+
+
+        public void Start()
+        {
+            if (text == null)
+            {
+                text = GetComponentInChildren<TMP_Text>();
+            }
+        }
+
+        private void Update()
+        {
+            if (text != null)
+            {
+                text.text = ItemAmount.ToString();
+            }
+        }
 
         public void spent()
         {
